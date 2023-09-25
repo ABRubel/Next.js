bb855000ad66d2d91c8c381f5a894b24de52b074
abrubel <abdur.rahaman.ewu082@gmail.com>
2023-09-26 01:04:09 +0600

    Layouts

---
+++ app/NavBar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-slate-200 p-5">
+      <Link href="/" className="mr-5">
+        Home
+      </Link>
+      <Link href="/users" className="">
+        Users
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
