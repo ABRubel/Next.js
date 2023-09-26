6817139e8600bda65c277aff81d67bbaa3770c8c
abrubel <abdur.rahaman.ewu082@gmail.com>
2023-09-27 00:39:40 +0600

    Add loader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div>
+      <span className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 loading loading-dots loading-lg"></span>
+    </div>
+  );
+};
+
+export default loading;
